1964382633b314c4c2956c17809c37f1961a521a
Bethany Norton <annielulu93@yahoo.com>
2018-02-01 06:50:01 +0000

    rails db:seed is NOT working

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  mount_uploader :picture, PictureUploader
+  
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
@@ -6,7 +8,6 @@ class Product < ApplicationRecord
   validates :image_url, allow_blank: true, format: {
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
-  }
-
+ }
 end
 
